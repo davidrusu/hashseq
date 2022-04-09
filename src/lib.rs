f8fa5b15bd8fef41b7fee3e5f657d4ed59c4c071
David Rusu <davidrusu.me@gmail.com>
2022-04-09 15:12:09 -0400

    2-3 tree implementation

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,10 @@ pub mod hashseq;
 // pub mod topo_after_and_before;
 pub mod topo_sort;
 // pub mod topo_sort_strong_weak;
-// pub mod tree;a
 pub mod cursor;
+pub mod tree;
 
-pub use self::hashseq::{HashSeq, HashNode};
 pub use self::cursor::Cursor;
+pub use self::hashseq::{HashNode, HashSeq};
 
 type Id = u64;
