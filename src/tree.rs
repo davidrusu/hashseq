648e20121fc726b2c303606aad2e38b5a5207c92
David Rusu <davidrusu.me@gmail.com>
2022-04-09 16:44:32 -0400

    feat(tree): fast position lookup

--- src/tree.rs
+++ src/tree.rs
@@ -24,6 +24,7 @@ struct Tree {
     /// we share pointers to the leaf nodes to avoid allocations
     leaf_idx: Index,
     id_to_node: BTreeMap<Id, Index>,
+    parent: BTreeMap<Index, Index>,
 }
 
 impl Default for NodeWithMeta {
@@ -44,6 +45,7 @@ impl Default for Tree {
             nodes,
             leaf_idx,
             id_to_node: BTreeMap::default(),
+            parent: BTreeMap::default(),
         }
     }
 }
@@ -58,20 +60,86 @@ impl Tree {
     }
 
     fn two_node(&mut self, left: Index, val: Id, right: Index) -> Index {
-        self.nodes.insert(NodeWithMeta {
+        let idx = self.nodes.insert(NodeWithMeta {
             node: Node::Two(left, val, right),
             size: self.nodes[left].size + 1 + self.nodes[right].size,
-        })
+        });
+        self.id_to_node.insert(val, idx);
+        self.parent.insert(left, idx);
+        self.parent.insert(right, idx);
+        idx
     }
 
     fn three_node(&mut self, l: Index, lv: Id, m: Index, rv: Id, r: Index) -> Index {
-        self.nodes.insert(NodeWithMeta {
+        let idx = self.nodes.insert(NodeWithMeta {
             node: Node::Three(l, lv, m, rv, r),
             size: self.nodes[l].size + 1 + self.nodes[m].size + 1 + self.nodes[r].size,
-        })
+        });
+        self.id_to_node.insert(lv, idx);
+        self.id_to_node.insert(rv, idx);
+        self.parent.insert(l, idx);
+        self.parent.insert(m, idx);
+        self.parent.insert(r, idx);
+        idx
+    }
+
+    fn position(&mut self, v: Id) -> Option<usize> {
+        let node_idx = *self.id_to_node.get(&v)?;
+        let mut position = match self.nodes[node_idx].node {
+            Node::Leaf => panic!("we shouldn't see any leaf"),
+            Node::Two(l, _, _) => self.nodes[l].size,
+            Node::Three(l, lv, m, rv, _) => {
+                if lv == v {
+                    self.nodes[l].size
+                } else {
+                    assert_eq!(rv, v);
+                    self.nodes[l].size + 1 + self.nodes[m].size
+                }
+            }
+        };
+
+        let mut child = node_idx;
+        loop {
+            if child == self.root {
+                return Some(position);
+            }
+
+            let parent = self.parent[&child];
+            match self.nodes[parent].node {
+                Node::Leaf => panic!("unexpected leaf"),
+                Node::Two(l, _, r) => {
+                    if child == l {
+                        // nothing to do
+                    } else {
+                        assert_eq!(child, r);
+                        position += self.nodes[l].size + 1;
+                    }
+                }
+                Node::Three(l, _, m, _, r) => {
+                    if child == l {
+                        // nothing to do
+                    } else if child == m {
+                        position += self.nodes[l].size + 1;
+                    } else {
+                        assert_eq!(child, r);
+                        position += self.nodes[l].size + 1 + self.nodes[m].size + 1;
+                    }
+                }
+            }
+
+            child = parent;
+        }
     }
 
     fn insert(&mut self, idx: usize, value: Id) {
+        if self.id_to_node.contains_key(&value) {
+            println!(
+                "Ignoring insert at {idx} of already inserted value {value} at {}",
+                self.position(value).unwrap()
+            );
+            return;
+        }
+
         match self.insert_rec(idx, value, 0, self.root) {
             Some((left, value, right)) => {
                 if self.root != self.leaf_idx {
@@ -106,6 +174,11 @@ impl Tree {
                     match self.insert_rec(idx, value, prefix_len, l) {
                         Some((cl, cv, cr)) => {
                             self.nodes[root].node = Node::Three(cl, cv, cr, v, r);
+                            self.id_to_node.insert(cv, root);
+                            self.id_to_node.insert(v, root);
+                            self.parent.insert(cl, root);
+                            self.parent.insert(cr, root);
+                            self.parent.remove(&l);
                         }
                         None => (),
                     }
@@ -113,6 +186,11 @@ impl Tree {
                     match self.insert_rec(idx, value, left_bound + 1, r) {
                         Some((cl, cv, cr)) => {
                             self.nodes[root].node = Node::Three(l, v, cl, cv, cr);
+                            self.id_to_node.insert(cv, root);
+                            self.id_to_node.insert(v, root);
+                            self.parent.insert(cl, root);
+                            self.parent.insert(cr, root);
+                            self.parent.remove(&r);
                         }
                         None => (),
                     }
@@ -284,21 +362,21 @@ mod tests {
     fn test_insert_at_end() {
         let mut tree = Tree::default();
 
-        tree.insert(0, 0);
-        tree.insert(1, 0);
+        tree.insert(0, 10);
+        tree.insert(1, 20);
 
-        assert_eq!(Vec::from_iter(tree.iter()), vec![0, 0]);
+        assert_eq!(Vec::from_iter(tree.iter()), vec![10, 20]);
     }
 
     #[test]
     fn test_insert_in_middle() {
         let mut tree = Tree::default();
 
-        tree.insert(0, 0);
-        tree.insert(0, 0);
-        tree.insert(1, 0);
+        tree.insert(0, 1);
+        tree.insert(0, 2);
+        tree.insert(1, 3);
 
-        assert_eq!(Vec::from_iter(tree.iter()), vec![0, 0, 0]);
+        assert_eq!(Vec::from_iter(tree.iter()), vec![2, 3, 1]);
     }
 
     #[test]
@@ -306,38 +384,38 @@ mod tests {
         let mut model = Vec::new();
         let mut tree = Tree::default();
 
-        model.insert(0, 0);
-        tree.insert(0, 0);
+        model.insert(0, 1);
+        tree.insert(0, 1);
 
         assert_eq!(tree.len(), 1);
 
-        model.insert(0, 0);
-        tree.insert(0, 0);
+        model.insert(0, 2);
+        tree.insert(0, 2);
 
         assert_eq!(tree.len(), 2);
 
-        model.insert(0, 0);
-        tree.insert(0, 0);
+        model.insert(0, 3);
+        tree.insert(0, 3);
 
         assert_eq!(tree.len(), 3);
 
-        model.insert(0, 0);
-        tree.insert(0, 0);
+        model.insert(0, 4);
+        tree.insert(0, 4);
 
         assert_eq!(tree.len(), 4);
 
-        model.insert(0, 0);
-        tree.insert(0, 0);
+        model.insert(0, 5);
+        tree.insert(0, 5);
 
         assert_eq!(tree.len(), 5);
 
-        model.insert(0, 0);
-        tree.insert(0, 0);
+        model.insert(0, 6);
+        tree.insert(0, 6);
 
         assert_eq!(tree.len(), 6);
 
-        model.insert(6, 0);
-        tree.insert(6, 0);
+        model.insert(6, 7);
+        tree.insert(6, 7);
 
         assert_eq!(tree.len(), 7);
 
@@ -349,11 +427,11 @@ mod tests {
         let mut model = Vec::new();
         let mut tree = Tree::default();
 
-        model.insert(0, 0);
-        tree.insert(0, 0);
+        model.insert(0, 1);
+        tree.insert(0, 1);
 
-        model.insert(0, 0);
-        tree.insert(0, 0);
+        model.insert(0, 2);
+        tree.insert(0, 2);
 
         assert_eq!(model.get(1).cloned(), tree.get(1));
 
@@ -363,13 +441,13 @@ mod tests {
     #[quickcheck]
     fn test_vec_model_qc4() {
         let inserts = [
-            (0, 0, 0),
-            (0, 0, 0),
-            (0, 0, 0),
-            (0, 0, 0),
-            (0, 0, 0),
-            (0, 0, 0),
-            (0, 0, 0),
+            (0, 0, 1),
+            (0, 0, 2),
+            (0, 0, 3),
+            (0, 0, 4),
+            (0, 0, 5),
+            (0, 0, 6),
+            (0, 0, 7),
             (1, 5, 0),
         ];
         let mut model = Vec::new();
@@ -468,24 +546,64 @@ mod tests {
         assert!(model.iter().copied().eq(tree.iter()));
     }
 
+    #[test]
+    fn test_vec_model_qc7() {
+        let mut model = Vec::new();
+        let mut tree = Tree::default();
+
+        model.insert(0, 0);
+        tree.insert(0, 0);
+
+        assert_eq!(tree.position(tree.get(0).unwrap()).unwrap(), 0);
+
+        assert!(model.iter().copied().eq(tree.iter()));
+    }
+
+    #[test]
+    fn test_vec_model_qc8() {
+        let mut model = Vec::new();
+        let mut tree = Tree::default();
+
+        model.insert(0, 0);
+        tree.insert(0, 0);
+
+        model.insert(0, 1);
+        tree.insert(0, 1);
+
+        dbg!(&tree);
+
+        assert_eq!(tree.position(tree.get(0).unwrap()).unwrap(), 0);
+
+        assert!(model.iter().copied().eq(tree.iter()));
+    }
+
     #[quickcheck]
     fn prop_vec_model(inserts: Vec<(u8, usize, Id)>) {
         let mut model = Vec::new();
         let mut tree = Tree::default();
 
         for (mut instruction, mut idx, value) in inserts {
-            instruction = instruction % 2;
+            instruction = instruction % 3;
 
             assert_eq!(idx.min(model.len()), idx.min(tree.len()));
-            idx = idx.min(tree.len());
+            idx = idx.min(tree.len().saturating_sub(1));
 
             match instruction {
                 0 => {
+                    if tree.position(value).is_some() {
+                        continue;
+                    }
                     model.insert(idx, value);
                     tree.insert(idx, value);
                 }
                 1 => {
                     assert_eq!(model.get(idx).cloned(), tree.get(idx))
+                }
+                2 => {
+                    if tree.is_empty() {
+                        continue;
+                    }
+                    assert_eq!(tree.position(tree.get(idx).unwrap()).unwrap(), idx)
                 }
                 i => panic!("Unexpected instruction {i}"),
             }
